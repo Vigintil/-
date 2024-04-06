2d805e037211905a64facb170c09bca45fff4b1b
Vigintil <157875614+Vigintil@users.noreply.github.com>
2024-04-06 09:52:49 +0400

    Add files via upload

---
+++ 1232/Classes/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1232
+{
+    static internal class Class1
+    {
+        static public int a;
+        static public int b;
+        static public int c;
+
+        public static int[] Calc()
+        {
+            int[] abc = new int[3];
+            abc[0] = a;
+            abc[1] = b;
+            abc[2] = c;
+            int buf;
+            if (abc[0] > abc[1] ) 
+            {
+                buf = abc[0];
+                abc[0] = abc[1];
+                abc[1] = buf;
+            }
+            if (abc[0] > abc[2])
+            {
+                buf = abc[0];
+                abc[0] = abc[2];
+                abc[2] = buf;
+            }
+            if (abc[1] > abc[2])
+            {
+                buf = abc[1];
+                abc[1] = abc[2];
+                abc[2] = buf;
+            }
+            return abc;
+        }
+    }
+}
---
+++ 1232/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _1232.Classes;
+
+namespace _1232
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Class1.a = int.Parse(textBox1.Text);
+                Class1.b = int.Parse(textBox2.Text);
+                Class1.c = int.Parse(textBox3.Text);
+                int[] abc = Class1.Calc();
+                button1.Text = "Calc";
+                textBox4.Text = $"{abc[0]}, {abc[1]}, {abc[2]}";
+            }
+            catch (Exception ex)
+            {
+                textBox4.Text = "Лох";
+                button1.Text = $"{ex}";
+            }
+        }
+    }
+}
